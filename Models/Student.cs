7b306ae20e728e7c31b7e4aaff7bf60b2f01cf64
FredericB19 <55847205+FredericB19@users.noreply.github.com>
2021-07-24 02:35:21 -0400

    Initial commit

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Lab6.Models
+{
+    public class Student : StudentBase
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Student Number")]
+
+        public Guid ID { 
+            get; 
+            set; 
+        }
+    }
+}
